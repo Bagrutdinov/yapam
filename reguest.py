e22b4cae2100f10a8e9766dca1e533263c0b9be9
artem_bag <bagrutdinov@mail.ru>
2020-11-24 19:40:48 +0300

    request modif

--- reguest.py
+++ reguest.py
@@ -1,1 +1,12 @@
-curl -i -H "Content-Type: application/json" -X POST -d '{"x": 10,"y": 20,"zoom": 17}' http://localhost:5000/yamap/api/v1.0/fromGlobalPixels
+#curl -i -H "Content-Type: application/json" -X POST -d '{"x": 10,"y": 20,"zoom": 17}' http://localhost:5000/yamap/api/v1.0/fromGlobalPixels
+
+import json
+import requests
+
+url = 'http://localhost:5000/yamap/api/v1.0/fromGlobalPixels'
+payload = {"x": 10,"y": 20,"zoom": 17}
+headers = {'content-type': 'application/json'}
+
+r = requests.post(url, data=json.dumps(payload), headers=headers)
+r_json = r.json()
+print(r_json)
